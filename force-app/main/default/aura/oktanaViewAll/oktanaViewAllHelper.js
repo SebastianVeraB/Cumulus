124d3c75db1f1ffd1e72fe72215f1f80033c5924
Sebastian Bucalo <sebastian.bucalo@oktana.io>
2020-06-30 10:46:14 -0300

    saving work

--- force-app/main/default/aura/oktanaViewAll/oktanaViewAllHelper.js
+++ force-app/main/default/aura/oktanaViewAll/oktanaViewAllHelper.js
@@ -1,12 +1,10 @@
 ({
     handleViewAll: function (component) {
-        console.log('event fire i');
         var handleViewEvent = component.getEvent("viewAllEvent");
         handleViewEvent.setParams({
             "viewAll": !component.get('v.viewingAll'),
             "key": "viewAll"
         });
         handleViewEvent.fire();
-        console.log('event fire f');
     }
 })
--- force-app/main/default/aura/oktanaViewContainer/oktanaViewContainerHelper.js
+++ force-app/main/default/aura/oktanaViewContainer/oktanaViewContainerHelper.js
@@ -1,19 +1,38 @@
 ({
     handleViewAll: function (component) {
-        var itemsToShow1 = component.get('v.itemsList1');
-        var itemsToShow2 = component.get('v.itemsList2');
 
         var viewMoreLess = 'View less';
 
         if (!component.get('v.viewAll')) {
-            //itemsToShow1 = itemsToShow1.slice(0, 3);
-            //itemsToShow2 = itemsToShow2.slice(0, 3);
-
             viewMoreLess = component.get('v.viewAllTitle');
         }
-
-        component.set('v.itemsToShow1', itemsToShow1);
-        component.set('v.itemsToShow2', itemsToShow2);
         component.set('v.viewMoreLess', viewMoreLess);
+    },
+
+    getNotifications: function (component) {
+        var action = component.get("c.RetrieveNotifications");
+
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                
+                console.log(response);
+                console.log(response.getReturnValue());
+                //component.set("v.itemsList1", response.getReturnValue());
+                
+            } else if (state === "ERROR") {
+                var errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " +
+                            errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        });
+
+        $A.enqueueAction(action);
     }
 })
